47604e360332f2a1dedd7c039421d20c01e3f960
Ville Joensuu <ville.joensuu@cgi.com>
2023-08-29 10:48:12 +0300

    Version 0.3.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', encoding='utf-8') as readme:
 
 setuptools.setup(
     name='qgis-stubs',
-    version='0.3.1',
+    version='0.3.2',
     url='https://github.com/leonhard-s/qgis-stubs',
     author='Leonhard S.',
     maintainer_email='leonhard-sei@outlook.com',
